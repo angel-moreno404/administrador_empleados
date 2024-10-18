a132e93623cf61c56270bbb295f9fccf4ec501cf
angel <thenewstart0111@gmail.com>
2024-10-18 09:24:50 -0400

    listo produccion

--- God/settings/base.py
+++ God/settings/base.py
@@ -3,6 +3,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).ancestor(3)
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
+#adding env file
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
---
+++ God/settings/prodution.py
@@ -0,0 +1,46 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+
+# import environ
+# env = environ.Env()
+# environ.Env.read_env()
+
+
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'dbempleado_again',
+#         'USER': 'postgres',
+#         'PASSWORD': '123456',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+#render postgress data base
+import dj_database_url
+
+    #'default':dj_database_url.parse('postgresql://produtiondatabase_user:if6ucxTXfI8sYPXERcKr3doIbKV7CiiX@dpg-cs8jce08fa8c73but0fg-a.ohio-postgres.render.com/produtiondatabase')
+DATABASES = {
+    # 'default':dj_database_url.parse(env('DATABASE_URL'))
+    'default':dj_database_url.parse('postgresql://produtiondatabase_user:if6ucxTXfI8sYPXERcKr3doIbKV7CiiX@dpg-cs8jce08fa8c73but0fg-a.ohio-postgres.render.com/produtiondatabase')
+
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
